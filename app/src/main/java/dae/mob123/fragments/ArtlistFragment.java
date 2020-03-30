bd753062bc94fd662c9ea6398fe9b5903092290f
Egon Bomon <egon.bomon2801@hotmail.com>
2020-03-30 11:28:24 +0200

    Basic structure added: - Blank fragments                        - Model package                        - Util package

---
+++ app/src/main/java/dae/mob123/fragments/ArtlistFragment.java
@@ -0,0 +1,29 @@
+package dae.mob123.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import dae.mob123.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ArtlistFragment extends Fragment {
+
+    public ArtlistFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_artlist, container, false);
+    }
+}

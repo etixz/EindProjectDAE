1999920f079844ae9391189dbd33001d6076a330
floatingreels <david.gunzburg@gmail.com>
2020-04-01 23:02:54 +0200

    Navigation to List fragment

--- app/src/main/java/dae/mob123/fragments/HomeFragment.java
+++ app/src/main/java/dae/mob123/fragments/HomeFragment.java
@@ -3,10 +3,12 @@ package dae.mob123.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import dae.mob123.R;
 
@@ -14,6 +16,15 @@ import dae.mob123.R;
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
+
+    private Button toListBtn;
+    private View.OnClickListener toListListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Navigation.findNavController(view).navigate(R.id.home_to_list);
+        }
+    };
 
     public HomeFragment() {
         // Required empty public constructor
@@ -24,6 +35,9 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+         toListBtn = rootView.findViewById(R.id.btn_home_list);
+         toListBtn.setOnClickListener(toListListener);
+        return rootView;
     }
 }

574da81a26791381d0f82a6b8b55a2f661010f55
baloutaki <amin.ahmadi.baloutaki@gmail.com>
2020-04-04 12:11:06 +0200

    ik heb een customize marker voorzien. ik heb die omgezet in een bitmap. ik heb ook de daarbij horende functies geschreven

--- app/src/main/java/dae/mob123/fragments/MapFragment.java
+++ app/src/main/java/dae/mob123/fragments/MapFragment.java
@@ -1,9 +1,18 @@
 package dae.mob123.fragments;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +29,7 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -29,6 +39,7 @@ import dae.mob123.R;
 import dae.mob123.model.Mural;
 import dae.mob123.model.MuralViewModel;
 
+
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -37,21 +48,27 @@ public class MapFragment extends Fragment {
     private MapView mapView;
     private GoogleMap myMap;
     private AppCompatActivity mycontext;
+    private LatLng markerLatLng;
+    private Marker customMarker;
 
     private OnMapReadyCallback onMapReady = new OnMapReadyCallback() {
+
 
         @Override
         public void onMapReady(GoogleMap googleMap) {
             //field maken om de googlemap instantie in andere methoden te krijgen
             myMap = googleMap;
+
             //kaart klaar
             //kaart centreren op een coördinaat.
-            LatLng coördBrussel = new LatLng(50.858712, 4.347446);
+            markerLatLng = new LatLng(50.858712, 4.347446);
 
-            CameraUpdate moveToBrussel = CameraUpdateFactory.newLatLngZoom(coördBrussel, 16);
+            CameraUpdate moveToBrussel = CameraUpdateFactory.newLatLngZoom(markerLatLng, 16);
 
             googleMap.animateCamera(moveToBrussel);
             //googleMap.setMapType(googleMap.MAP_TYPE_SATELLITE);
+
+            mycontext.setContentView(R.layout.custom_marker_layout);
 
             myMap.setOnInfoWindowClickListener(infoWindowClickListener);
             setMarkerAdapter();
@@ -59,86 +76,137 @@ public class MapFragment extends Fragment {
         }
     };
 
-        private GoogleMap.OnInfoWindowClickListener infoWindowClickListener = new GoogleMap.OnInfoWindowClickListener() {
+    private GoogleMap.OnInfoWindowClickListener infoWindowClickListener = new GoogleMap.OnInfoWindowClickListener() {
+        @Override
+        public void onInfoWindowClick(Marker marker) {
+            Mural mural = (Mural) marker.getTag();
+            if (mural != null)
+                //TODO: Bundle aanmaken, serializable te steken en doorsturen met navigatie naar Detail
+                Toast.makeText(getActivity(), mural.getCharacter(), Toast.LENGTH_SHORT).show();
+
+        }
+
+    };
+
+
+    private void setMarkerAdapter() {
+        myMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
             @Override
-            public void onInfoWindowClick(Marker marker) {
-                Mural mural = (Mural) marker.getTag();
-                if (mural != null)
-                    //TODO: Bundle aanmaken, serializable te steken en doorsturen met navigatie naar Detail
-                    Toast.makeText(getActivity(), mural.getCharacter(), Toast.LENGTH_SHORT).show();
+            public View getInfoWindow(Marker marker) {
+                return null;
             }
-        };
 
-        private void setMarkerAdapter() {
-            myMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoContents(Marker marker) {
+                View cardView = getActivity().getLayoutInflater().inflate(R.layout.mural_card, null, false);
+                TextView tvCharacter = cardView.findViewById(R.id.tv_card_mural_character);
+                TextView tvArtistYear = cardView.findViewById(R.id.tv_card_mural_artistyear);
+                TextView tvAddress = cardView.findViewById(R.id.tv_card_mural_address);
 
+                cardView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Navigation.findNavController(v).navigate(R.id.detailFragment);
+                    }
+                });
+
+
+                tvCharacter.setText(marker.getTitle());
+                tvArtistYear.setText(marker.getTitle());
+                tvAddress.setText(marker.getTitle());
+
+
+                return cardView;
+            }
+        });
+    }
+
+
+    private void drawMarkers() {
+
+        View marker = getActivity().getLayoutInflater().inflate(R.layout.custom_marker_layout, null);
+        customMarker = myMap.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromBitmap(createDrawableFromView(mycontext, marker))));
+
+        final View mapView = getChildFragmentManager().findFragmentById(R.id.mapFragment).getView();
+        if (mapView.getViewTreeObserver().isAlive()) {
+            mapView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @SuppressLint("NewApi")
+
+                // nakijken welke build version dat wij aan gebruiken zijn.
                 @Override
-                public View getInfoWindow(Marker marker) {
-                    return null;
-                }
-
-                @Override
-                public View getInfoContents(Marker marker) {
-                    View cardView = getActivity().getLayoutInflater().inflate(R.layout.mural_card, null, false);
-                    TextView tvCharacter = cardView.findViewById(R.id.tv_card_mural_character);
-                    TextView tvArtistYear = cardView.findViewById(R.id.tv_card_mural_artistyear);
-                    TextView tvAddress = cardView.findViewById(R.id.tv_card_mural_address);
-
-                    cardView.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Navigation.findNavController(v).navigate(R.id.detail_fragment);
-                        }
-                    });
-
-                    tvCharacter.setText(marker.getTitle());
-                    tvArtistYear.setText(marker.getTitle());
-                    tvAddress.setText(marker.getTitle());
-
-                    return cardView;
+                public void onGlobalLayout() {
+                    LatLngBounds bounds = new LatLngBounds.Builder().include(markerLatLng).build();
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                        mapView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                    } else {
+                        mapView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    }
+                    myMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
                 }
             });
         }
 
-        private void drawMarkers() {
-            myMap.addMarker(new MarkerOptions()
-                    .icon(BitmapDescriptorFactory.defaultMarker()));
+        MuralViewModel muralViewModel = new ViewModelProvider(mycontext).get(MuralViewModel.class);
+        //TODO: wanneer lijst met Murals omgezet is naar LiveData, hier Observer aanmaken en onChanged() overschrijven
+        muralViewModel.getMurals().observe(mycontext, new Observer<List<Mural>>() {
+            @Override
+            public void onChanged(List<Mural> murals) {
+                for (Mural mural : murals) {
+                    Marker m = myMap.addMarker(new MarkerOptions()
+                            .position(mural.getCoordinates()));
 
-            MuralViewModel muralViewModel = new ViewModelProvider(mycontext).get(MuralViewModel.class);
-            //TODO: wanneer lijst met Murals omgezet is naar LiveData, hier Observer aanmaken en onChanged() overschrijven
-            muralViewModel.getMurals().observe(mycontext, new Observer<List<Mural>>() {
-                @Override
-                public void onChanged(List<Mural> murals) {
-                    for (Mural mural : murals) {
-                        Marker m = myMap.addMarker(new MarkerOptions()
-                                .position(mural.getCoordinates()));
-
-                        m.setTitle(mural.getCharacter());
-                        m.setTitle(mural.getYear());
-                        m.setTitle(String.valueOf(mural.getCoordinates()));
-                        m.setTag(mural);
+                    m.setTitle(mural.getCharacter());
+                    m.setTitle(mural.getYear());
+                    m.setTitle(String.valueOf(mural.getCoordinates()));
+                    m.setTag(mural);
                 }
-            }});
-        }
+            }
+        });
+    }
 
 
-        public MapFragment() {
-            // Required empty public constructor
-        }
+    // omzetten van een view in een bitmap
+    public static Bitmap createDrawableFromView(Context mycontext, View mapView) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) mycontext).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        mapView.setLayoutParams(new ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT));
+        mapView.measure(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        mapView.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
+        mapView.buildDrawingCache();
+        Bitmap bitmap = Bitmap.createBitmap(mapView.getMeasuredWidth(), mapView.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        mapView.draw(canvas);
+
+        return bitmap;
+    }
 
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            // Inflate the layout for this fragment
-            View rootView = inflater.inflate(R.layout.fragment_map, container, false);
-            mapView = rootView.findViewById(R.id.mapView);
-            mapView.onCreate(savedInstanceState);
-            mapView.getMapAsync(onMapReady);
+    public MapFragment() {
+        // Required empty public constructor
+    }
 
 
-            return rootView;
-        }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+        mapView = rootView.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(onMapReady);
+
+
+        return rootView;
+    }
+
+
+
+
         @Override
         public void onResume() {
             super.onResume();

aef4be8641464a2a31cf8cca845babbc88a1c505
floatingreels <david.gunzburg@gmail.com>
2020-04-14 00:52:19 +0200

    Added placeholder image for Detail screen if not connected to internet

--- app/src/main/java/dae/mob123/fragments/DetailFragment.java
+++ app/src/main/java/dae/mob123/fragments/DetailFragment.java
@@ -1,6 +1,7 @@
 package dae.mob123.fragments;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,8 +15,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.material.button.MaterialButton;
 import com.squareup.picasso.Picasso;
 
@@ -24,6 +23,7 @@ import dae.mob123.fragments.util.LocationConverter;
 import dae.mob123.model.Mural;
 
 import static dae.mob123.model.util.MuralType.COMIC_BOOK;
+import static dae.mob123.model.util.MuralType.STREET_ART;
 
 //Author:DG
 public class DetailFragment extends Fragment {
@@ -33,7 +33,8 @@ public class DetailFragment extends Fragment {
     private Bundle dataFromList;
     private ImageView imageIV;
     private AppCompatActivity appCompatActivity;
-    private GoogleMap myMap;
+    private ConnectivityManager connectivityManager;
+    private MaterialButton showOnMap;
 
 
     public DetailFragment() {
@@ -42,44 +43,47 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView =  inflater.inflate(R.layout.fragment_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
         characterTV = rootView.findViewById(R.id.tv_detail_character);
         artistYearTV = rootView.findViewById(R.id.tv_detil_artistyear);
         streetAddressTV = rootView.findViewById(R.id.tv_detail_street);
         imageIV = rootView.findViewById(R.id.iv_detail_img);
-
-        if (dataPassed()){
+        showOnMap = rootView.findViewById(R.id.btn_show_place_on_map);
+        if (dataPassed()) {
             muralFromList = (Mural) dataFromList.getSerializable("mural_to_detail");
             characterTV.setText(muralFromList.getCharacterTitle());
             artistYearTV.setText(muralFromList.getArtist() + ", " + muralFromList.getYear());
             LocationConverter myConverter = new LocationConverter();
             streetAddressTV.setText(myConverter.convertCoordinatesToAddress(appCompatActivity, muralFromList.getCoordinates()));
             if (muralFromList.getMuralType() == COMIC_BOOK) {
-                Picasso.get().load("https://opendata.bruxelles.be/api/v2/catalog/datasets/comic-book-route/files/" + muralFromList.getImageID()).into(imageIV);
+                Picasso.get().load("https://opendata.bruxelles.be/api/v2/catalog/datasets/comic-book-route/files/" + muralFromList.getImageID())
+                        .placeholder(R.drawable.ic_signal_wifi_off_grey_24dp)
+                        .into(imageIV);
             } else {
-                Picasso.get().load("https://opendata.brussel.be/api/v2/catalog/datasets/street-art/files/" + muralFromList.getImageID()).into(imageIV);
-            };
+                Picasso.get().load("https://opendata.brussel.be/api/v2/catalog/datasets/street-art/files/" + muralFromList.getImageID())
+                        .placeholder(R.drawable.ic_signal_wifi_off_grey_24dp)
+                        .into(imageIV);
+            }
         }
-        MaterialButton showOnMap = rootView.findViewById(R.id.btn_show_place_on_map);
+
         showOnMap.setOnClickListener(new View.OnClickListener() {
-            private Marker marker;
             @Override
             public void onClick(View view) {
-                    Bundle data = new Bundle();
-                    data.putSerializable("mural_to_map", muralFromList);
-                    Navigation.findNavController(view).navigate(R.id.detail_to_map, data);
-                }
-
+                Bundle data = new Bundle();
+                data.putSerializable("mural_to_map", muralFromList);
+                Navigation.findNavController(view).navigate(R.id.detail_to_map, data);
+            }
         });
 
         return rootView;
     }
 
+
     public boolean dataPassed() {
         dataFromList = getArguments();
         if (dataFromList != null) {
-            if (dataFromList.containsKey("mural_to_detail")){
+            if (dataFromList.containsKey("mural_to_detail")) {
                 return true;
             }
         }
@@ -92,5 +96,5 @@ public class DetailFragment extends Fragment {
         appCompatActivity = (AppCompatActivity) context;
     }
 
-
 }
+
